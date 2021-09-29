ef35574ff64f99eff980f90d6b0b1d624245632e
ilmannafian04 <milmannafian04@gmail.com>
2021-09-29 10:02:49 +0700

    feat: added skeleton ui

--- ui/src/routes/index.svelte
+++ ui/src/routes/index.svelte
@@ -1,1 +1,14 @@
-<h1>Hello</h1>
+<script lang="ts">
+  let code = '';
+  let joinHandler = () => {
+    console.log(code);
+  };
+</script>
+
+<h1>Sweeprs</h1>
+<button>Create</button>
+<span>- OR -</span>
+<form on:submit|preventDefault={joinHandler}>
+  <input bind:value={code} />
+  <button type="submit">Join</button>
+</form>
